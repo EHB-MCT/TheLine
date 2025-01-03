b8c8f16510b160d2d91be6e260288345213604e8
ALEXANDERMERSSEMAN <92094191+ALEXANDERMERSSEMAN@users.noreply.github.com>
2025-01-03 21:40:55 +0100

    feat(authentication): implement login and sign-up system with REST API and MongoDB
    Added UI components for user login and sign-up functionality. Integrated with a REST API for user authentication and MongoDB for data persistence.

--- Assets/Scripts/GeneralGame/GameManagement/StartGame.cs
+++ Assets/Scripts/GeneralGame/GameManagement/StartGame.cs
@@ -1,55 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using MongoDB.Driver;
-using System.IO; // Voor Path en File
-using Newtonsoft.Json; // Voor JsonConvert
 
 public class StartGame : MonoBehaviour
 {
-    private MongoClient client;
-    private IMongoDatabase database;
-    private string connectionString;
-
-    void Start()
-    {
-        // Laad configuratie voor de databaseverbinding
-        LoadConfiguration();
-        client = new MongoClient(connectionString);
-        database = client.GetDatabase("TheLine");
-    }
-
     public void StartGameProcess()
     {
-        if (SignIn.isLoggedIn)
+        if (Login.IsLoggedIn)
         {
             Debug.Log("Starting the game...");
             SceneManager.LoadScene(2); // Laad het eerste level
         }
         else
         {
-            Debug.LogError("You must be logged in to start the game!");
-        }
-    }
-
-    private void LoadConfiguration()
-    {
-        string configFilePath = Path.Combine(Application.streamingAssetsPath, "config.json");
-
-        if (File.Exists(configFilePath))
-        {
-            string json = File.ReadAllText(configFilePath);
-            Config config = JsonConvert.DeserializeObject<Config>(json);
-
-            connectionString = config.mongoConnectionString;
-
-            if (string.IsNullOrEmpty(connectionString))
-            {
-                Debug.LogError("Connection string is empty!");
-            }
-        }
-        else
-        {
-            Debug.LogError("Config file not found!");
+            Debug.LogWarning("You must log in before starting the game.");
         }
     }
 }
